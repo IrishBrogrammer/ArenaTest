b2f708c7d997bb91fb81ec1d38352c43fc1b5407
Brian McAleese <bmcaleese12@gmail.com>
2016-04-06 16:21:05 +0100

    Added a script to generate the unique card ids present so only textures that are used are downloaded and stored in an images folder

---
+++ config/generateTextureDict.py
@@ -0,0 +1,33 @@
+import os,glob
+import sys,getopt
+import json
+import os.path
+
+
+
+# Go through all the configs and generate a json file that contains the ids of all the cards in the configs
+def main():
+
+    picks = []
+    for subdir, dirs , files in os.walk( "." ) :
+        for filez in [ f for f in files if f.endswith(".json") ] :
+            fullPath = os.path.join( subdir, filez)
+
+            with open( fullPath ) as data_file :
+                data = json.load( data_file )
+                draftPicks =  data["draftPicks"]
+
+                for pick in draftPicks :
+                    options = pick["options"]
+
+                    for card in options :
+                        picks.append( card )
+
+
+    print "Runnig test "
+    uniques = set(picks)
+    for newCard in uniques :
+        print newCard
+
+
+main();
