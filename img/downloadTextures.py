1ce3e29d310752bed6b32bee90077ef85a439465
Brian McAleese <bmcaleese12@gmail.com>
2016-04-08 15:56:52 +0100

    Added image folder and first test image,small script to downloan image and removed file that is no longer used

---
+++ img/downloadTextures.py
@@ -0,0 +1,19 @@
+
+
+
+def runCardCheck( ) :
+
+
+
+
+def main() :
+  # Location of json file that stores the card id to url
+  cardLibrary = str( sys.argv[0] )
+  # Location of json file that stores all the cards ids present in the configs
+  cardsUsed = str( sys.argv[1] )
+
+
+
+
+
+main()
---
+++ scripts/python/cardTextDownloader.py
@@ -0,0 +1,4 @@
+import urllib
+
+
+urllib.urlretrieve("http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_621.png" , "test.png")
