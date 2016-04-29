8ffa5e1893baf3de927ffc495e392592a1d860e3
Brian McAleese <bmcaleese12@gmail.com>
2016-04-29 16:45:17 +0100

    start of script to download missing textures

--- scripts/python/cardTextDownloader.py
+++ scripts/python/cardTextDownloader.py
@@ -1,4 +1,19 @@
 import urllib
 
+def downloadCard( card ) : 
+    name = card["name"]
+    filename = "cards/" + name + ".png"
+    url = card["url"]
+    urllib.urlretrieve( url , filename )
 
-urllib.urlretrieve("http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_621.png" , "test.png")
+def main() : 
+    card = {}
+    card["name"] = "testing"
+    card["url"] = "http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_621.png"
+    downloadCard( card )
+
+
+main()
+
+
+
