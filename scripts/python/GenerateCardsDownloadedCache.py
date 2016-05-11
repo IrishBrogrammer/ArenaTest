0ad6066fddc384b7a912883a4727b5ac81c76fd8
Brian McAleese <bmcaleese12@gmail.com>
2016-05-11 12:59:38 +0100

    Small clean up

--- scripts/python/GenerateCardsDownloadedCache.py
+++ scripts/python/GenerateCardsDownloadedCache.py
@@ -5,13 +5,17 @@ import json
 # Go through a sub folder of images and generate a json list of all the card names
 # so we have a list of cards already downloaded 
 
-def main() :
+def main( cardFolder , outputFile ) :
     downloadedCards = []
-    for subdir,dirs,files in os.walk( "cards" ) : 
+    for subdir,dirs,files in os.walk( cardFolder ) : 
         for filez in [ f for f in files if f.endswith(".png") ] :     
             downloadedCards.append( filez[:-4])
     
-    with open( "downloadedTextures.json" , 'w' ) as f : 
+    with open( outputFile , 'w' ) as f : 
         f.write( json.dumps( downloadedCards , sort_keys=True , indent=4 , separators=(',',':') ))
     
-main()
+    
+    
+cardFolder = str( sys.argv[1] )
+outputFile = str( sys.argv[2] )    
+main( cardFolder , outputFile )
