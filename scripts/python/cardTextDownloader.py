48210616fe84212a89807b519fc9d36866024cac
Brian McAleese <bmcaleese12@gmail.com>
2016-05-11 14:41:43 +0100

    added script to download missing image files

--- scripts/python/cardTextDownloader.py
+++ scripts/python/cardTextDownloader.py
@@ -1,10 +1,10 @@
 import urllib
+import json 
+import sys
 
-def downloadCard( card ) : 
-    name = card["name"]
-    filename = "cards/" + name + ".png"
-    url = card["url"]
-    urllib.urlretrieve( url , filename )
+def downloadCard( cardName , cardURL ) : 
+    filename = "cards/" + cardName + ".png"
+    urllib.urlretrieve( cardURL , filename )
 
 def main() : 
     card = {}
@@ -13,7 +13,55 @@ def main() :
     downloadCard( card )
 
 
-main()
+
+def downloadMissingCards( missingCards , urlDict ) :
+
+    for card in missingCards :
+        if card in urlDict :
+            downloadCard( card , urlDict[card] )
+        else :
+            print "Could not find" + card 
+    
+
+def generateMissingCardsList( validCards , cachedCards ) :
+    missingCards = []
+    missingCards = set(validCards) - set(cachedCards)
+    return missingCards
+
+def downloadCards( validCardsPath , downloadedCardsPath ,  cardDictPath   ) :
+    
+    validCards = []
+    cachedCards = []
+    urlDict = []
+    
+    with open( validCardsPath ) as data_file : 
+        validCards = json.load( data_file )
+    
+    with open( downloadedCardsPath ) as cached_file : 
+        cachedCards = json.load( cached_file )
+        
+    missingCards = generateMissingCardsList( validCards , cachedCards )  
+    
+    print " The following cards are missing " 
+    for card in missingCards :
+        print card
+    
+    print " Attempting to download missing cards " 
+    with open( cardDictPath ) as url_file : 
+        urlDict = json.load( url_file )
+     
+    downloadMissingCards( missingCards , urlDict )
+    
+   # with open( cardDictPath ) as card_file : 
+    #    cardDict = json.load( cardDict )
+        
+        
+
+
+validCardsPath = str( sys.argv[1])
+cachedCardsPath = str( sys.argv[2] )
+cardDict = str( sys.argv[3] )
+downloadCards( validCardsPath , cachedCardsPath , cardDict )
 
 
 
